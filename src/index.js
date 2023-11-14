4fc8b61ef9a2a536490a097882113240ef62a94f
Mithlesh Mehta <mehtamithlesh9@gmail.com>
2023-11-14 21:59:57 +0545

    mongoose connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({path: './env'});
+connectDB()
+
+
+
+/*
+import { express } from "express";
+const app = express();
 
 ( async () => {
     try {
-        mongoose.connect(`${process.env.MONGODB_URI}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("error", error)
+            throw error
+        });
+        app.listen(process.env.PORT, () =>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
     } catch (error) {
         console.log("Error:", error)
         throw err
         
     }
 })()
+*/
