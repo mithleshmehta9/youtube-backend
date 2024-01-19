45d99569bfbd79d1e2cdb04a9d5fc24a4d024bbc
mithleshmehta9 <97396166+mithleshmehta9@users.noreply.github.com>
2024-01-20 00:06:28 +0545

    Update index.js
    db change

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () =>{
     try {
-        const connectionInstance = await mongoose.connect(`mongodb+srv://mehtamithlesh9:Mehta98242@mithlesh9.eesnep5.mongodb.net/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${DB_NAME}`);
         console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MONGODB connection field ", error);
